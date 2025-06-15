a3ac80c24470ce399476782578a9747b39456572
ahmed-elgihamy <ahmedelghmay9781@gmail.com>
2025-06-15 13:39:32 +0300

    Project Structure

---
+++ src/app/components/payments/escrow-details/escrow-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escrow-details',
+  imports: [],
+  templateUrl: './escrow-details.component.html',
+  styleUrl: './escrow-details.component.css'
+})
+export class EscrowDetailsComponent {
+
+}
---
+++ src/app/components/subscriptions/subscription-plans/subscription-plans.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-plans',
+  imports: [],
+  templateUrl: './subscription-plans.component.html',
+  styleUrl: './subscription-plans.component.css'
+})
+export class SubscriptionPlansComponent {
+
+}
