bf1d4dd02116757266e3700418944a9f5e1239ff
Mohamed <Mohamed@DESKTOP-DVRVIHK>
2025-06-26 09:50:22 +0300

    adding guard and login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,58 +1,77 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    RouterModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   loginForm: FormGroup;
   formSubmitted = false;
+  errorMessage = '';
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
-      remember: [false]
+      remember: [false],
     });
   }
 
-  get f() { return this.loginForm.controls; }
+  get f() {
+    return this.loginForm.controls;
+  }
 
   getErrorMessage(controlName: string) {
     const control = this.f[controlName];
-    
+
     if (!this.formSubmitted && !control.dirty) return '';
-    
+
     if (control.hasError('required')) {
       return 'This field is required';
     }
-    
-    switch(controlName) {
+
+    switch (controlName) {
       case 'email':
         return 'Invalid email format';
     }
-    
+
     return '';
   }
 
   onSubmit(): void {
     this.formSubmitted = true;
-    
+
     if (this.loginForm.invalid) {
       return;
     }
-
     console.log('Login data:', this.loginForm.value);
-    this.router.navigate(['']);
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+
+      error: (err) => {
+        this.errorMessage = 'Invalid email or password. Please try again.';
+        console.error('Login failed', err);
+      },
+    });
+    // this.router.navigate(['']);
   }
 }
