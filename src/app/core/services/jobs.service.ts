3f38ed0cfb35f1c9fb50d0c8a376c2cf53afad62
Mohamed <Mohamed@DESKTOP-DVRVIHK>
2025-06-30 00:49:21 +0300

    adding proposal

--- src/app/core/services/jobs.service.ts
+++ src/app/core/services/jobs.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PagedResponse } from '../../types/PagedResponse';
 import { ApiResponse } from '../../types/ApiResponse';
+import { CreateProposalDTO } from '../../types/Proposals/CreateProposalDTO';
 
 @Injectable({
   providedIn: 'root',
@@ -29,5 +30,9 @@ export class JobsService {
 
   CreateJob(data: any): Observable<ApiResponse<any>> {
     return this.http.post<ApiResponse<any>>(`${this.apiUrl}/job/create`, data);
+  }
+
+  ApplyToJob(data: CreateProposalDTO): Observable<ApiResponse<any>> {
+    return this.http.post<ApiResponse<any>>(`${this.apiUrl}/proposals`, data);
   }
 }
