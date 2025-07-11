e66c8770560bf6c51e02f5e6b3a3565b7d3e7f55
rawanysoliman <rawanysoliman@gmail.com>
2025-07-11 15:28:47 +0300

    Update env.ts

--- src/app/core/env/env.ts
+++ src/app/core/env/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   baseUrl: 'https://localhost:44302/api',
   publicUrl: 'https://localhost:44302',
-  publicimageUrl: 'https://localhost:44302'
+  publicImgUrl: 'https://localhost:44302'
 };
 
